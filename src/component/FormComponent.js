6c64a91a5a2347c9f3df2ab22e08753456190a82
Bigorian <100525479+Bigorian@users.noreply.github.com>
2023-04-06 06:57:21 +0700

    insert_preventDefault

--- src/component/FormComponent.js
+++ src/component/FormComponent.js
@@ -6,17 +6,25 @@ const inputTitle = (event) =>{
     
 }
 
+// inputAmount
+const inputAmount = (event) =>{
+    console.log(event.target.value)
+}
 
+const saveItem = (event) =>{
+    event.preventDefault()
+    console.log("บันทึกข้อมูลเรียบร้อย")
+}
 
     return (
-        <form>
+        <form onSubmit={saveItem}>
             <div className="form-control">
                 <label>ชื่อรายการ</label>
                 <input type="text" placeholder="ระบุชื่อรายการของคุณ" onChange={inputTitle} />
             </div>
             <div className="form-control">
                 <label>จำนวนเงิน</label>
-                <input type="number" placeholder="(+ รายรับ , - รายจ่าย)" />
+                <input type="number" placeholder="(+ รายรับ , - รายจ่าย)" onChange={inputAmount}/>
             </div>
             <div>
                 <button type='submit' className='btn'>
