4fe8424e89b2142b40157637e90fa629addc90a4
Bigorian <100525479+Bigorian@users.noreply.github.com>
2023-04-08 08:53:40 +0700

    Clear_State_and_attached_variable_in_tag

--- src/component/FormComponent.js
+++ src/component/FormComponent.js
@@ -1,39 +1,45 @@
 import './FormComponent.css';
 import { useState } from 'react';
 const Form_Component = () => {
+    // input useState
+    const [title, setTitle] = useState('');
+    const [amount, setAmount] = useState(0);
+
 
 
     // inputTitle
-const inputTitle = (event) =>{
-    setTitle(event.target.value)
-    
-}
+    const inputTitle = (event) => {
+        setTitle(event.target.value)
 
-// inputAmount
-const inputAmount = (event) =>{
-    setAmount(event.target.value)
-}
+    }
 
-const saveItem = (event) =>{
-    event.preventDefault()
-    console.log("บันทึกข้อมูลเรียบร้อย")
-}
+    // inputAmount
+    const inputAmount = (event) => {
+        setAmount(event.target.value)
+    }
 
+    const saveItem = (event) => {
+        event.preventDefault() // ใช้ในกรณีเมื่อกดปุ่ม Submit แล้วค่าจะถูกบันทึกไม่หายไป
+        const itemData = {
+            ee: title,
+            aa: amount
+        }
+        console.log(itemData);
+        setTitle('');
+        setAmount(0);
+    }
 
-// input useState
-const [title,setTitle] = useState('');
-const [amount,setAmount] = useState(0);
 
 
     return (
         <form onSubmit={saveItem}>
             <div className="form-control">
                 <label>ชื่อรายการ</label>
-                <input type="text" placeholder="ระบุชื่อรายการของคุณ" onChange={inputTitle} />
+                <input type="text" placeholder="ระบุชื่อรายการของคุณ" onChange={inputTitle} value={title} />
             </div>
             <div className="form-control">
                 <label>จำนวนเงิน</label>
-                <input type="number" placeholder="(+ รายรับ , - รายจ่าย)" onChange={inputAmount}/>
+                <input type="number" placeholder="(+ รายรับ , - รายจ่าย)" onChange={inputAmount} value={amount} />
             </div>
             <div>
                 <button type='submit' className='btn'>
