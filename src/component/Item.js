dcf9e1d47811a90d40758e5660c5739fe2d2b5c2
Bigorian <100525479+Bigorian@users.noreply.github.com>
2023-04-14 12:24:48 +0700

    Create_Method_status_inside_item

--- src/component/Item.js
+++ src/component/Item.js
@@ -3,7 +3,21 @@ import './Item.css';
 
 
 const Item = (props) => {
+   
+
+
+
+
+
     const {title,amount} = props  // เขียนเป็น Distructuring
+
+
+
+     //สถานะการเงินที่ส่งเข้ามาแต่ละรายการ
+     const status = amount<0 ?  "รายจ่าย":"รายรับ"  //เขียนแบบ Ternary
+
+
+
     return (
         <div className='list-box'>
             <ul>
